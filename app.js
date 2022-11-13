eedce3cc92f5f5493bbbf7dd5018920422c14847
Nikita Miller <melscre4m@gmail.com>
2022-11-13 18:21:56 +0200

    create server

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import config from './config/main.js';
+import router from './routes/routes.js';
+
+const app = express();
+
+app.use(router);
+
+https
+	.createServer(
+		{
+			key: fs.readFileSync(config.PATH_TO_KEY),
+			cert: fs.readFileSync(config.PATH_TO_CERT)
+		},
+		app
+	)
+	.listen(config.PORT, () => console.log(`https://localhost:${config.PORT}`));
