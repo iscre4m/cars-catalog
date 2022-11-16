0ba4808a089d80ea69f803201edd1da18909d208
Nikita Miller <melscre4m@gmail.com>
2022-11-16 10:56:52 +0200

    add routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	res.send('ok');
-});
+router
+	.route('/')
+	.get((req, res) => res.send('all cars'))
+	.post((req, res) => res.send('car was added'));
+
+router
+	.route('/:id')
+	.get((req, res) => res.send(`car ${req.params.id}`))
+	.put((req, res) => res.send('car was edited'))
+	.delete((req, res) => res.send('car was removed'));
 
 export default router;
