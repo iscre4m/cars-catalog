1ce26b884271c6448b78f7caa0f4c8be14c1aa11
Nikita Miller <melscre4m@gmail.com>
2022-11-17 17:32:55 +0200

    add post request

--- models/car.js
+++ models/car.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
-import { manufacturerSchema } from './manufacturer.js';
 
-const carSchema = new mongoose.Schema({
-	name: { type: String, minLength: 2 },
-	manufacturer: { type: manufacturerSchema, required: true },
-	yearManufactured: { type: Number, min: 1880 },
-	engineVolume: Number,
-	price: { type: Number, required: true },
-	color: { type: String, required: true },
-	description: String
-});
+const carSchema = new mongoose.Schema(
+	{
+		name: { type: String, minLength: 2 },
+		manufacturer: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Manufacturer',
+			required: true
+		},
+		yearManufactured: { type: Number, min: 1880 },
+		engineVolume: Number,
+		price: { type: Number, required: true },
+		color: { type: String, required: true },
+		description: String
+	},
+	{ versionKey: false }
+);
 
 const Car = mongoose.model('Car', carSchema);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,35 @@
 import { Router } from 'express';
+import mongoose from 'mongoose';
 import filter from '../middleware/filter.js';
+import Car from '../models/car.js';
+import Manufacturer from '../models/manufacturer.js';
 
 const router = Router();
+const connection = mongoose.connection;
 
 router
 	.route('/')
 	.get(filter, (req, res) => res.send('all cars'))
-	.post((req, res) => res.send('car was added'));
+	.post((req, res) => {
+		Manufacturer.findOneAndUpdate(
+			{ name: req.body.manufacturer.name },
+			req.body.manufacturer,
+			{
+				upsert: true,
+				new: true
+			}
+		)
+			.then(result => {
+				req.body.manufacturer = result;
+				Car.create(req.body).then(() => {
+					res.send('car was added');
+				});
+			})
+			.catch(err => {
+				console.log(`failed to insert car: ${err}`);
+				res.send('error, try again later');
+			});
+	});
 
 router
 	.route('/:id')
